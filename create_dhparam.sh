1c6cf6b1a724884f3c235edb0fe40beabf41b9d4
Fabian Köster <mail@fabian-koester.com>
2017-02-11 13:00:39 +0100

    Use separate dir for dhparam

--- create_dhparam.sh
+++ create_dhparam.sh
@@ -1,6 +1,6 @@
-DHPARAM_FILE="/etc/ssl/dhparam.pem"
+DHPARAM_FILE="/etc/dhparam/dhparam.pem"
 if [ ! -f ${DHPARAM_FILE} ]; then
-
     echo "dhparam file ${DHPARAM_FILE} does not exist. Generating one with 4086 bit. This will take a while..."
+    mkdir -p $(dirname ${DHPARAM_FILE})
     openssl dhparam -out ${DHPARAM_FILE} 4096 && echo "Finished. Starting service now..."
 fi
